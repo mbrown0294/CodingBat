d7b30eccf0e809cc2886b67e36a57b0e3dba5c4f
mbrown0294 <32070444+mbrown0294@users.noreply.github.com>
2017-09-18 12:20:09 -0400

    feat: add diff21

--- CodingBat1.py
+++ CodingBat1.py
@@ -1,2 +1,10 @@
 def sleep_in(weekday, vacation):
     return (not weekday) or vacation
+
+
+def diff21(n):
+    diff = n - 21
+    if diff > 0:
+        return (diff) * 2
+    else:
+        return diff * -1
