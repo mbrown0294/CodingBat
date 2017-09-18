4663430ca46d8519321d81a12d944dc40d64573b
brownmr1 <mbrown0294@gmail.com>
2017-09-18 12:50:20 -0400

    feat: add near-hundred

--- CodingBat1.py
+++ CodingBat1.py
@@ -8,3 +8,12 @@ def diff21(n):
         return (diff) * 2
     else:
         return diff * -1
+
+
+def near_hundred(n):
+    diff1 = n - 100
+    diff2 = n - 200
+    if (abs(diff1) <= 10) | (abs(diff2) <= 10):
+        return True
+    else:
+        return False
