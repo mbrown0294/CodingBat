1e332b1a71215999062aa53b3f43b1f894f89ddd
brownmr1 <mbrown0294@gmail.com>
2017-09-18 14:40:18 -0400

    feat: add monkey_trouble

--- CodingBat1.py
+++ CodingBat1.py
@@ -38,3 +38,10 @@ def front3(str):
         return str + str + str
     chunk = str[:3]
     return chunk + chunk + chunk
+
+
+def monkey_trouble(a_smile, b_smile):
+  if (a_smile and b_smile) or not (a_smile or b_smile):
+    return True
+  else:
+    return False
