e81a74745014b3b7ac046f09d164349a652532a6
brownmr1 <mbrown0294@gmail.com>
2017-09-20 12:41:24 -0400

    feat: add array_count9

--- CodingBat2.py
+++ CodingBat2.py
@@ -42,3 +42,11 @@ def last2(str):
     if str[x:x +2] == phr:
       count += 1
   return count
+
+
+def array_count9(nums):
+  count = 0
+  for x in nums:
+    if x == 9:
+      count += 1
+  return count
