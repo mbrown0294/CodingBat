c0dd4208b24cf35f80839145b22e9ba88b20516d
Brown <brownmr1@jhuapl.edu>
2017-09-25 11:35:54 -0400

    feat:add date-fashion

--- CodingBat5.py
+++ CodingBat5.py
@@ -8,3 +8,12 @@ def cigar_party(cigars, is_weekend):
   return rep
 
 
+def date_fashion(you, date):
+  rep = 1
+  if (you >= 8) | (date >= 8):
+    rep = 2
+  if(you <= 2) | (date <=2):
+    rep = 0
+  return rep
+
+
