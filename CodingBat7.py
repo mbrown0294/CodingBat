02e5eeff9c04d2e06f6ea58e69804d718eb1abdf
Brown <brownmr1@jhuapl.edu>
2017-09-29 11:39:35 -0400

    feat:add count-code

--- CodingBat7.py
+++ CodingBat7.py
@@ -24,3 +24,11 @@ def cat_dog(str):
     return countC == countD
 
 
+def count_code(str):
+  count = 0
+  for x in range(len(str) - 3):
+    if (str[x:x+2] == "co") & (str[x+3] == "e"):
+      count += 1
+  return count
+
+
