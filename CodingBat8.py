78311dd64d2793d714b875fd4d365098a9720396
Brown <brownmr1@jhuapl.edu>
2017-09-29 12:15:03 -0400

    feat:add big-diff

--- CodingBat8.py
+++ CodingBat8.py
@@ -6,3 +6,14 @@ def count_evens(nums):
   return count
 
 
+def big_diff(nums):
+    large = nums[0]
+    small = nums[0]
+    for x in nums:
+        if x > large:
+            large = x
+        if x < small:
+            small = x
+    return large - small
+
+
